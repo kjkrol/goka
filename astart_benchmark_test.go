ab82b421a377768b3d2f11ce9ef2472b44d6f3ec
kjkrol <kjkrol@gmail.com>
2026-05-20 14:31:03 +0200

    test: Add benchamrks

---
+++ astart_benchmark_test.go
@@ -0,0 +1,64 @@
+package goka
+
+import (
+	"iter"
+	"math"
+	"testing"
+)
+
+// Make sure you have imported the "testing" package at the top of the file, e.g.:
+// import (
+// 	"iter"
+// 	"math"
+// 	"testing"
+// )
+
+func BenchmarkAStar(b *testing.B) {
+	const size = 64
+
+	// 1. Setup - initialize the grid before starting the timer
+	grid := make([][]float64, size)
+	for y := range size {
+		grid[y] = make([]float64, size)
+		for x := range size {
+			grid[y][x] = float64(((x + y) % 5) + 1)
+		}
+	}
+
+	start := Point{X: 0, Y: 0}
+	goal := Point{X: 63, Y: 63}
+
+	heuristic := func(p Point) float64 {
+		return math.Abs(float64(goal.X-p.X)) + math.Abs(float64(goal.Y-p.Y))
+	}
+
+	cost := func(p Point) float64 {
+		return grid[p.Y][p.X]
+	}
+
+	next := func(p Point) iter.Seq[Point] {
+		return func(yield func(Point) bool) {
+			dirs := []Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+			for _, d := range dirs {
+				nx, ny := p.X+d.X, p.Y+d.Y
+				if nx >= 0 && nx < size && ny >= 0 && ny < size {
+					if !yield(Point{X: nx, Y: ny}) {
+						return
+					}
+				}
+			}
+		}
+	}
+
+	// 2. Reset the timer!
+	// This ensures the time needed to generate the map is not included in the result.
+	b.ResetTimer()
+
+	// 3. Benchmark loop
+	// Go will automatically adjust the value of b.N so the test runs long enough
+	// (usually about 1 second) for reliable results.
+	for i := 0; i < b.N; i++ {
+		astar := NewAStar(start, goal, heuristic, cost, next)
+		_ = astar.Run() // We don't care about the returned result in the benchmark
+	}
+}
